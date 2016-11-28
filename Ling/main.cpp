a36ca2ec3c229bc0e594eaad19e7e72112268709
Vladislav Korzun <sdernal2@gmail.com>
2016-11-28 13:32:46 +0300

    Try to use tokrnizer, but failed.

--- Ling/main.cpp
+++ Ling/main.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 #include <freeling/morfo/lang_ident.h>
-#include <string>
-#include <freeling/morfo/dates.h>
-using namespace std;
+#include <freeling/morfo/tokenizer.h>
+#include <freeling/morfo/splitter.h>
+//#include <freeling/morfo/maco.h>
 
-int main() {
-	freeling::lang_ident f;
-	std::wstring kek = f.identify_language(L"I like it");
-	cout << kek.c_str() << endl;
-	freeling::dates date(L"default");
-	freeling::word w1(L"12/12/1993");
-	freeling::sentence s;
-	s.push_back(w1);
-	date.analyze(s);
+using namespace std;
+using namespace freeling;
+
+int main(int argc, char** argv) {
+	tokenizer *tk = NULL;
+//	splitter  *sp = NULL;
+//	maco *morfo = NULL;
+
+	util::init_locale(L"default");
+	wstring	ipath = L"/usr/local/share/freeling/en/";
+	wstring tokenizer_path = L"/usr/local/share/freeling/en/tokenizer.dat";
+	wstring splitter_path = L"/usr/local/share/freeling/en/splitter.dat";
+	tk = new tokenizer(tokenizer_path);
+//	sp = new splitter(splitter_path);
+	wstring text = L"That is, once created, the tokenizer module receives plain text in a string, tokenizes it, and returns a list of word objects corresponding to the created tokens";
+	std::list<word> words = tk->tokenize(text);
+	const word::const_iterator alt;
+	for (auto word = words.begin(); word != words.end(); word++) {
+		wcout << word->get_lemma();
+	}
 	return 0;
 }
